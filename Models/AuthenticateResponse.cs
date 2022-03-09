29697953f755f63ab2d7ca25cfe98f01271566c5
bharath-developer <96855386+bharath-developer@users.noreply.github.com>
2022-03-09 15:20:21 +0530

    added type to signup response

--- Models/AuthenticateResponse.cs
+++ Models/AuthenticateResponse.cs
@@ -7,10 +7,12 @@ public class AuthenticateResponse
   public int Id { get; set; }
   public string Username { get; set; }
   public string Token { get; set; }
+  public string Type { get; set; }
   public AuthenticateResponse(TblUser user, string token)
   {
     Id = user.Id;
     Username = user.Username;
     Token = token;
+    Type = user.Type;
   }
 }
